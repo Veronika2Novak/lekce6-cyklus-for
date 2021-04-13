7def58c49cc8bce3c77d2558bbdb05aaed236b8d
Veronika Novakova <veronika3novak@gmail.com>
2021-04-13 20:09:07 +0200

    Final

--- main.js
+++ main.js
@@ -3,29 +3,33 @@
 
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
-console.log('První příklad - vzestupná řada 0 - 10');
 
-//TADY NAPIS CYKLUS
+let cislaDoDeseti = [];
+for (let i=1; i <= 10; i = i+1) {
+    cislaDoDeseti.push(i);
+}
 
-console.log('-------------------');
+console.log(cislaDoDeseti);
 
-
+let vypsanaCisla = document.querySelector("#vypsat-cisla");
+vypsanaCisla.innerHTML = cislaDoDeseti;
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
 
-console.log('Druhý příklad - rozdíl mezi <= a <');
+let cislaMensiDeseti = [];
+for (let i=1; i < 10; i = i+1) {
+    cislaMensiDeseti.push(i);
+}
 
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
-
-
+console.log(cislaMensiDeseti);
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
-console.log('Třetí příklad - sestupná řada 10 - 0');
 
-//TADY NAPIS CYKLUS
+let cislaSestupne = [];
+for (let i=10; i > 0; i = i-1) {
+    cislaSestupne.push(i);
+}
 
-console.log('-------------------');
+console.log(cislaSestupne);
